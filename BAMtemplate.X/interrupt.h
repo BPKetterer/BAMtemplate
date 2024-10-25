cb0d3b66e1bb0ccba1298a1126684ccffff64436
BPKetterer <BenniKe2000@gmail.com>
2024-10-25 20:48:48 +0200

    added inline documentation to interrupt.h

--- BAMtemplate.X/interrupt.h
+++ BAMtemplate.X/interrupt.h
@@ -3,6 +3,8 @@
  * Author: Benni
  *
  * Created on October 12, 2024, 2:05 PM
+ * 
+ * This header manages the global en-/dis-ablement of interrupts
  */
 
 #ifndef INTERRUPT_H
@@ -11,8 +13,14 @@
 #include <avr/interrupt.h>
 #include "config.h"
 
+/*
+ * Allows all interrupts.
+ */
 void interrupt_global_enable(void);
 
+/*
+ * Prevents all interrupts.
+ */
 void interrupt_global_disable(void);
 
 #endif	/* INTERRUPT_H */
