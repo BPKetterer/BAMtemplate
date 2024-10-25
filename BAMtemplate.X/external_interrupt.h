5fa7f8ad108e5169c15039ab5a0e0a27c6ae9d6a
BPKetterer <BenniKe2000@gmail.com>
2024-10-25 20:46:38 +0200

    added inline documentation to external_interrupt.h

--- BAMtemplate.X/external_interrupt.h
+++ BAMtemplate.X/external_interrupt.h
@@ -3,6 +3,8 @@
  * Author: Benni
  *
  * Created on October 2, 2024, 12:49 PM
+ * 
+ * This header allows easy usage of the external interrupts
  */
 
 #ifndef EXTERNAL_INTERRUPT_H
@@ -14,18 +16,49 @@
 #include "port.h"
 #include "error.h"
 
+/*
+ * Initiates some stuff for the external interrupts
+ */
 void external_interrupt_init(void);
 
-void external_interrupt_enable(port port, uint8_t pin);
-
-void external_interrupt_disable(port port, uint8_t pin);
-
-void external_interrupt_enable_masked(port port, uint8_t mask);
-
-void external_interrupt_disable_masked(port port, uint8_t mask);
-
+/*
+ * Sets the function that is called by the port, when an external interrupt is
+ * triggered.
+ * @param   port            The port for which the function is used
+ * @param   funciton        The function that is called when the interrupt is
+ *                          triggered
+ * @param   interrupt_data  A pointer to data that is passed to the interrupt
+ *                          function
+ */
 void external_interrupt_set_function(port port, void (*function)(void *), void * interrupt_data);
 
+/*
+ * Enables an external interrupt of a specific pin.
+ * @param   port    The port of the pin
+ * @param   pin     The pin
+ */
+void external_interrupt_enable(port port, uint8_t pin);
+
+/*
+ * Disables an external interrupt of a specific pin.
+ * @param   port    The port of the pin
+ * @param   pin     The pin
+ */
+void external_interrupt_disable(port port, uint8_t pin);
+
+/*
+ * Enables external interrupts for a port with a pin mask.
+ * @param   port    The port for which the mask is
+ * @param   mask    The mask of the pins that are enabled
+ */
+void external_interrupt_enable_masked(port port, uint8_t mask);
+
+/*
+ * Disables external interrupts for a port with a pin mask.
+ * @param   port    The port for which the mask is
+ * @param   mask    The mask of the pins that are disabled
+ */
+void external_interrupt_disable_masked(port port, uint8_t mask);
 
 #endif	/* EXTERNAL_INTERRUPT_H */
 
